a3a441f680ae31868e0e29e21c02366a2f9cc53d
dcep93 <dan@quizlet.com>
2022-06-14 11:57:55 -0700

    x

--- .github/workflows/deploy_backend_to_modal.sh
+++ .github/workflows/deploy_backend_to_modal.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 MODAL_KEY="$1"
 
-bash ./install_modal "$MODAL_KEY"
+bash ./install_modal.sh "$MODAL_KEY"
 
 cd ../../backend/first2know
 modal app deploy --name=first2know_server modal_app.py:modal_app
--- .github/workflows/deploy_cron_to_modal.sh
+++ .github/workflows/deploy_cron_to_modal.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 MODAL_KEY="$1"
 # TWITTER_KEY="$2"
 
-bash ./install_modal "$MODAL_KEY"
+bash ./install_modal.sh "$MODAL_KEY"
 
 cd ../../cron/first2know
 modal app deploy --name first2know_cron modal_app.py:modal_app
