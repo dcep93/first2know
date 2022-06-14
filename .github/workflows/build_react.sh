7ff857f031e83331f6aa91299b52c8039f1fbf27
dcep93 <dan@quizlet.com>
2022-06-14 10:26:53 -0700

    x

--- .github/workflows/build_react.sh
+++ .github/workflows/build_react.sh
@@ -3,9 +3,6 @@
 set -euo pipefail
 
 cd ../../frontend/first2know
-git diff .
-echo hi
-exit 1
 npm install
 yarn build
 rm -rf node_modules
--- .github/workflows/deploy_frontend.sh
+++ .github/workflows/deploy_frontend.sh
@@ -6,6 +6,9 @@ SA_KEY="$1"
 
 cd "$(dirname "$0")"
 
+git diff ../../frontend/first2know
+echo hi
+exit 1
 bash ./record_sha.sh "export const recorded_sha = \`%s\n%s\`;\n" "../../frontend/first2know/src/first2know/recorded_sha.tsx"
 bash ./build_react.sh
 bash ./deploy_to_firebase.sh "$SA_KEY"
