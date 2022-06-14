f3d040cbf90682736d9f5ede55d521fb34fc0ed4
dcep93 <dan@quizlet.com>
2022-06-14 10:30:00 -0700

    x

--- .github/workflows/deploy_backend.sh
+++ .github/workflows/deploy_backend.sh
@@ -6,5 +6,10 @@ MODAL_KEY="$1"
 
 cd "$(dirname "$0")"
 
+DIFF="$(git diff ../../backend/first2know)"
+if [[ -z "$DIFF" ]]; then
+  echo "no diff"
+  exit 0
+fi
 bash ./record_sha.sh "export const recorded_sha = \`%s\n%s\`;\n" "frontend/first2know/src/first2know/recorded_sha.tsx"
 bash ./deploy_to_modal.sh "$MODAL_KEY"
--- .github/workflows/deploy_frontend.sh
+++ .github/workflows/deploy_frontend.sh
@@ -6,9 +6,11 @@ SA_KEY="$1"
 
 cd "$(dirname "$0")"
 
-git diff ../../frontend/first2know
-echo hi
-exit 1
+DIFF="$(git diff ../../frontend/first2know)"
+if [[ -z "$DIFF" ]]; then
+  echo "no diff"
+  exit 0
+fi
 bash ./record_sha.sh "export const recorded_sha = \`%s\n%s\`;\n" "../../frontend/first2know/src/first2know/recorded_sha.tsx"
 bash ./build_react.sh
 bash ./deploy_to_firebase.sh "$SA_KEY"
