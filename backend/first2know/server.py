0fdfea2652362fe422debd28826ec4e5eb1ed672
dcep93 <dan@quizlet.com>
2022-06-15 10:50:08 -0700

    post_screenshot

--- backend/first2know/server.py
+++ backend/first2know/server.py
@@ -2,6 +2,7 @@ import asyncio
 import base64
 import io
 import traceback
+import typing
 
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, StreamingResponse
@@ -23,37 +24,17 @@ async def get_encrypt(data: str):
     return HTMLResponse(firebase_wrapper.encrypt(data))
 
 
-@web_app.get("/echo/{url:path}")
-async def get_echo(url: str):
-    return HTMLResponse(url)
-
-
-@web_app.get("/screenshot/{url:path}")
-async def get_screenshot(url: str):
+@web_app.post("/screenshot")
+async def post_screenshot(
+    url: str,
+    timeout: float=60.0,
+    css_selector: typing.Optional[str]=None,
+    fetch_params: typing.Dict[str, str]={},
+):
     try:
-        img_data = await asyncio.wait_for(screenshot.screenshot(url, None, {}), 60.0)
+        img_data = await asyncio.wait_for(screenshot.screenshot(url, css_selector, fetch_params), timeout)
         img_bytes = base64.b64decode(img_data)
         return StreamingResponse(io.BytesIO(img_bytes), media_type="image/png")
-    except Exception:
-        traceback.print_exc()
-        return None
-
-
-@web_app.get("/screenshot_info/{url:path}")
-async def get_screenshot_info(url: str):
-    try:
-        img_data = await asyncio.wait_for(screenshot.screenshot(url, None, {}), 60.0)
-        return len(img_data)
-    except Exception:
-        traceback.print_exc()
-        return None
-
-
-@web_app.get("/screenshot_raw/{url:path}")
-async def get_screenshot_raw(url: str):
-    try:
-        img_data = await asyncio.wait_for(screenshot.screenshot(url, None, {}), 60.0)
-        return HTMLResponse(img_data)
     except Exception:
         traceback.print_exc()
         return None
