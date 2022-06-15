85474ce0e9f17dcaf2bde9f24bdd5a64af743666
dcep93 <dan@quizlet.com>
2022-06-15 10:43:48 -0700

    x

--- .github/workflows/deploy_to_modal.sh
+++ .github/workflows/deploy_to_modal.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-set -xeuo pipefail
+set -euo pipefail
 
 MODAL_KEY="$1"
 # TODO dcep93 - save to modal
 # TWITTER_KEY="$2"
 
 TOKEN_ID="ak-38vXak6m5VJheg1cvrKQul"
-
-pip install --force-reinstall https://modal.com/api/client-library/us-JOXmaxhr5FVrM66sBK1J29/modal-py3-none-any.whl
-
 # TODO dcep93 - constant url
-MODAL_PIP_URL="https://modal.com/api/client-library/us-JOXmaxhr5FVrM66sBK1J29/modal-py3-none-any.whl"
+MODAL_PIP_URL="https://modal.com/api/client-library/us-JOXmaxhr5FVrM66sBK1J29/modal-0.0.17-py3-none-any.whl"
 
 pip install "$MODAL_PIP_URL"
 modal token set --token-id "$TOKEN_ID" --token-secret "$MODAL_KEY"
