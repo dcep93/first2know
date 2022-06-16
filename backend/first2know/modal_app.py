6ce90ccc916570daf327798536cffc2f384af012
dcep93 <dan@quizlet.com>
2022-06-16 16:36:29 -0700

    x

--- backend/first2know/modal_app.py
+++ backend/first2know/modal_app.py
@@ -6,15 +6,17 @@ if not modal.is_local():
     from . import cron
     from . import server
 
-image =  modal.DebianSlim()
-image.run_commands([
+image =  modal.DebianSlim().run_commands([
     "apt-get install -y software-properties-common",
     "apt-add-repository non-free",
     "apt-add-repository contrib",
     "apt-get update",
+]).run_commands([
     "pip install playwright==1.20.0",
     "playwright install-deps chromium",
     "playwright install chromium",
+]).run_commands([
+    'apt install -y git',
 ]).pip_install([
     'git+https://github.com/ozgur/python-firebase',
     'cryptography',
@@ -32,6 +34,7 @@ def init_client_secret():
     client_secret = os.environ["client_secret"]
     cron.Vars.client_secret = client_secret
 
-@modal_app.asgi(image=image)
+@modal_app.asgi(secret=modal.ref("first2know_s"))
 def app():
+    init_client_secret()
     return server.web_app
