29197d3fcec2689f1087fa4a1882b4707975c120
dcep93 <dan@quizlet.com>
2022-06-16 17:11:11 -0700

    x

--- .github/workflows/deploy_to_modal.sh
+++ .github/workflows/deploy_to_modal.sh
@@ -14,5 +14,8 @@ modal token set --token-id "$TOKEN_ID" --token-secret "$MODAL_KEY"
 cd ../../backend
 # TODO dcep93 set secret
 echo modal secret set "first2know" "client_secret" "$TWITTER_KEY"
-alias d="modal app deploy --name=first2know first2know.modal_app:modal_app"
+
+function d() {
+  modal app deploy --name=first2know first2know.modal_app:modal_app
+}
 d || d || d
