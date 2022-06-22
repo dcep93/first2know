c67ccbf6773f2c5b2a9f72a31b125c48d456794e
dcep93 <dan@quizlet.com>
2022-06-22 10:48:31 -0700

    x

--- backend/first2know/screenshot.py
+++ backend/first2know/screenshot.py
@@ -1,4 +1,5 @@
 import base64
+import collections
 import time
 import typing
 
@@ -6,6 +7,7 @@ from pydantic import BaseModel
 
 
 class RequestPayload(BaseModel):
+    key: typing.Optional[str]
     url: str
     cookie: typing.Optional[str] = None
     params: typing.Optional[typing.Dict[str, str]] = None
@@ -18,21 +20,28 @@ class ResponsePayload(BaseModel):
     evaluate: typing.Optional[str]
 
 
+def make_p():
+    return Vars.p_f()
+
+
 class Vars:
-    _p: typing.Any
+    p_f: typing.Any = None
+    ps = collections.defaultdict(make_p)
 
 
 def init():
     # https://playwright.dev/python/docs/intro
     from playwright.sync_api import sync_playwright as p  # type: ignore
-    Vars._p = p()
+    Vars.p_f = p
 
 
 def screenshot(payload: RequestPayload) -> ResponsePayload:
-    if Vars._p is None:
+    if Vars.p_f is None:
         return None  # type: ignore
 
     start = time.time()
+
+    p = make_p() if payload.key is None else Vars.ps[payload.key]
 
     params = {} if payload.params is None else dict(payload.params)
     if payload.cookie is not None:
@@ -43,47 +52,46 @@ def screenshot(payload: RequestPayload) -> ResponsePayload:
         "withing",
     )
 
-    with Vars._p as p:  # 1.2 seconds
-        print(
-            time.time() - start,
-            "Fetching url",
-            payload.url,
-        )
-        browser = p.chromium.launch()  # 3.9 seconds
-        print(
-            time.time() - start,
-            "paging",
-        )
-        page = browser.new_page()  # 0.8 seconds
-        page.set_extra_http_headers(params)
-        print(
-            time.time() - start,
-            "going to",
-        )
-        page.goto(payload.url)  # 0.3 seconds
-        print(
-            time.time() - start,
-            "evaluating",
-        )
-        evaluate = None if payload.evaluate is None else str(
-            page.evaluate(payload.evaluate))
-        locator = page if payload.selector is None else page.locator(
-            payload.selector)
-        print(
-            time.time() - start,
-            "screenshotting",
-        )
-        locator.screenshot(path="screenshot.png")  # 0.2 seconds
-        print(
-            time.time() - start,
-            "closing",
-        )
-        browser.close()
-        data = open("screenshot.png", "rb").read()
-        print(
-            time.time() - start,
-            f"Screenshot of size {len(data)} bytes",
-            f"from {payload.url}",
-        )
-        data = base64.b64encode(data).decode('utf-8')
-        return ResponsePayload(data=data, evaluate=evaluate)
+    print(
+        time.time() - start,
+        "Fetching url",
+        payload.url,
+    )
+    browser = p.chromium.launch()  # 3.9 seconds
+    print(
+        time.time() - start,
+        "paging",
+    )
+    page = browser.new_page()  # 0.8 seconds
+    page.set_extra_http_headers(params)
+    print(
+        time.time() - start,
+        "going to",
+    )
+    page.goto(payload.url)  # 0.3 seconds
+    print(
+        time.time() - start,
+        "evaluating",
+    )
+    evaluate = None if payload.evaluate is None else str(
+        page.evaluate(payload.evaluate))
+    locator = page if payload.selector is None else page.locator(
+        payload.selector)
+    print(
+        time.time() - start,
+        "screenshotting",
+    )
+    locator.screenshot(path="screenshot.png")  # 0.2 seconds
+    print(
+        time.time() - start,
+        "closing",
+    )
+    browser.close()
+    data = open("screenshot.png", "rb").read()
+    print(
+        time.time() - start,
+        f"Screenshot of size {len(data)} bytes",
+        f"from {payload.url}",
+    )
+    data = base64.b64encode(data).decode('utf-8')
+    return ResponsePayload(data=data, evaluate=evaluate)
