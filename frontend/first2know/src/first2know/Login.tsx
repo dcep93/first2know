7fcc14800a1f1f56ebf75d57d55d59a0f6396ca5
dcep93 <dan@quizlet.com>
2022-06-24 18:01:19 -0700

    x

--- frontend/first2know/src/first2know/Login.tsx
+++ frontend/first2know/src/first2know/Login.tsx
@@ -1,35 +1,9 @@
-import _TwitterLogin from "./TwitterLogin";
+import TwitterLogin from "./TwitterLogin";
 
 import React from "react";
 import { url } from "./Server";
 
 type UserType = { screen_name: string; user_id: string };
-
-class TwitterLogin extends _TwitterLogin {
-  openPopup() {
-    // @ts-ignore
-    const w = this.props.dialogWidth;
-    // @ts-ignore
-    const h = this.props.dialogHeight;
-    // const left = screen.width / 2 - w / 2;
-    // const top = screen.height / 2 - h / 2;
-    const left = 0;
-    const top = 0;
-
-    return window.open(
-      "",
-      "",
-      "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=" +
-        w +
-        ", height=" +
-        h +
-        ", top=" +
-        top +
-        ", left=" +
-        left
-    );
-  }
-}
 
 class Login extends React.Component<{}, { user: UserType | null }> {
   constructor(props: {}) {
