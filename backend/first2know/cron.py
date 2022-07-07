202b6ef00736deca54d0e4c7169984c2b07e4fc8
dcep93 <dan@quizlet.com>
2022-07-07 10:28:28 -0700

    x

--- backend/first2know/cron.py
+++ backend/first2know/cron.py
@@ -8,7 +8,7 @@ from . import screenshot
 from . import twitter_wrapper
 
 # update version to clear errors
-VERSION = '1.0'
+VERSION = '1.1'
 
 NUM_SCREENSHOTTERS = 8
 
--- frontend/first2know/src/first2know/ToHandle.tsx
+++ frontend/first2know/src/first2know/ToHandle.tsx
@@ -1,4 +1,4 @@
-import React, { createRef } from "react";
+import { createRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   AllToHandleType,
@@ -6,7 +6,8 @@ import {
   ToHandleType,
   UserType,
 } from "./firebase";
-import ImgRenderer from "./ImgRenderer";
+import loading from "./loading.gif";
+import { url } from "./Server";
 
 const urlRef = createRef<HTMLInputElement>();
 const cookieRef = createRef<HTMLInputElement>();
@@ -20,140 +21,155 @@ type SubmitType = (
   data_input: DataInputType & { old_encrypted: string | null }
 ) => Promise<string>;
 
-type PropsType = {
+function ToHandle(props: {
   user: UserType;
   toHandle?: ToHandleType;
   submit: SubmitType;
   allToHandle: AllToHandleType;
-};
+}) {
+  const [img_data, update] = useState<string | null | undefined>(undefined);
+  const navigate = useNavigate();
 
-class ToHandle extends React.Component<PropsType, { raw_img_data: string }> {
-  render() {
-    const defaultParamsValue = this.props.toHandle?.data_input.params;
-    return (
-      <div>
-        <form
-          onSubmit={(e) =>
-            Promise.resolve(e.preventDefault())
-              .then(() => this.getData())
-              .catch((err) => {
-                alert(err);
-                throw err;
+  const defaultParamsValue = props.toHandle?.data_input.params;
+  return (
+    <div>
+      <form
+        onSubmit={(e) =>
+          Promise.resolve(e.preventDefault())
+            .then(() => update(null))
+            .then(() => getData(props.toHandle))
+            .then((data) => JSON.stringify(data))
+            .then((body) =>
+              fetch(`${url}/screenshot`, {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body,
               })
-          }
-        >
+            )
+            .catch((err) => {
+              update(undefined);
+              alert(err);
+              throw err;
+            })
+        }
+      >
+        <div>
+          url:{" "}
+          <input
+            ref={urlRef}
+            defaultValue={props.toHandle?.data_input.url}
+            type="text"
+          />
+        </div>
+        <div title={"will be encrypted"}>
+          cookie: <input ref={cookieRef} type="text" />
+        </div>
+        {props.toHandle && (
           <div>
-            url:{" "}
-            <input
-              ref={urlRef}
-              defaultValue={this.props.toHandle?.data_input.url}
-              type="text"
+            reuse saved cookie?<span> </span>
+            <input ref={reuseCookieRef} defaultChecked type="checkbox" />
+          </div>
+        )}
+        <div>
+          params:{" "}
+          <input
+            ref={paramsRef}
+            defaultValue={
+              defaultParamsValue === null
+                ? undefined
+                : JSON.stringify(defaultParamsValue)
+            }
+            type="text"
+          />
+        </div>
+        <div>
+          css_selector:{" "}
+          <input
+            ref={cssSelectorRef}
+            defaultValue={props.toHandle?.data_input.selector || undefined}
+            type="text"
+          />
+        </div>
+        <div>
+          js_evaluate: {"("}transform evaluation to img
+          <input
+            onChange={() =>
+              (cssSelectorRef.current!.disabled =
+                evaluationToImgRef.current!.checked)
+            }
+            defaultChecked={props.toHandle?.data_input.evaluation_to_img}
+            ref={evaluationToImgRef}
+            type="checkbox"
+          />
+          {")"}
+          <div>
+            <textarea
+              defaultValue={props.toHandle?.data_input.evaluate || undefined}
+              ref={evaluateRef}
             />
           </div>
-          <div title={"will be encrypted"}>
-            cookie: <input ref={cookieRef} type="text" />
-          </div>
-          {this.props.toHandle && (
-            <div>
-              reuse saved cookie?<span> </span>
-              <input ref={reuseCookieRef} defaultChecked type="checkbox" />
-            </div>
-          )}
-          <div>
-            params:{" "}
-            <input
-              ref={paramsRef}
-              defaultValue={
-                defaultParamsValue === null
-                  ? undefined
-                  : JSON.stringify(defaultParamsValue)
-              }
-              type="text"
-            />
-          </div>
-          <div>
-            css_selector:{" "}
-            <input
-              ref={cssSelectorRef}
-              defaultValue={
-                this.props.toHandle?.data_input.selector || undefined
-              }
-              type="text"
-            />
-          </div>
-          <div>
-            js_evaluate: {"("}transform evaluation to img
-            <input
-              onChange={() =>
-                (cssSelectorRef.current!.disabled =
-                  evaluationToImgRef.current!.checked)
-              }
-              defaultChecked={this.props.toHandle?.data_input.evaluation_to_img}
-              ref={evaluationToImgRef}
-              type="checkbox"
-            />
-            {")"}
-            <div>
-              <textarea
-                defaultValue={
-                  this.props.toHandle?.data_input.evaluate || undefined
-                }
-                ref={evaluateRef}
-              />
-            </div>
-          </div>
-          <input type="submit" value="Check Screenshot" />
-        </form>
-        <ImgRenderer img_data={this.state?.raw_img_data} />
-        <SubmitableButton
-          onSubmit={(navigate) => this.onSubmit(this.props.submit, navigate)}
-        />
-      </div>
-    );
-  }
-
-  onSubmit(submit: SubmitType, navigate: (key: string) => void) {
-    Promise.resolve()
-      .then(() => this.getData())
-      .then((data_input) => submit(data_input))
-      .then((key) => navigate(key))
-      .catch((err) => {
-        alert(err);
-        throw err;
-      });
-  }
-
-  getData(): DataInputType & { old_encrypted: string | null } {
-    const old_encrypted =
-      this.props.toHandle === undefined
-        ? null
-        : reuseCookieRef.current?.checked
-        ? this.props.toHandle.encrypted
-        : null;
-    const paramsJson = paramsRef.current!.value || null;
-    const params = paramsJson ? JSON.parse(paramsJson) : {};
-    const cookie = cookieRef.current!.value || null;
-    if (cookie) params.cookie = cookie;
-    const data_input = {
-      url: urlRef.current!.value,
-      params,
-      selector: cssSelectorRef.current!.value || null,
-      evaluation: null,
-      evaluate: evaluateRef.current!.value || null,
-      evaluation_to_img: evaluationToImgRef.current!.checked,
-    };
-    if (data_input.url === "") {
-      throw Error("need to have a url");
-    }
-    return { old_encrypted, ...data_input };
-  }
+        </div>
+        <input type="submit" value="Check Screenshot" />
+      </form>
+      <img
+        src={
+          img_data === undefined
+            ? undefined
+            : img_data === null
+            ? loading
+            : `data:image/png;base64,${img_data}`
+        }
+        alt=""
+      ></img>
+      <button onClick={() => onSubmit(props.toHandle, props.submit, navigate)}>
+        Submit
+      </button>
+    </div>
+  );
 }
 
-function SubmitableButton(props: {
-  onSubmit: (navigate: (key: string) => void) => void;
-}) {
-  const navigate = useNavigate();
-  return <button onClick={() => props.onSubmit(navigate)}>Submit</button>;
+function onSubmit(
+  toHandle: ToHandleType | undefined,
+  submit: SubmitType,
+  navigate: (key: string) => void
+) {
+  Promise.resolve()
+    .then(() => getData(toHandle))
+    .then((data_input) => submit(data_input))
+    .then((key) => navigate(key))
+    .catch((err) => {
+      alert(err);
+      throw err;
+    });
+}
+
+function getData(
+  toHandle: ToHandleType | undefined
+): DataInputType & { old_encrypted: string | null } {
+  const old_encrypted =
+    toHandle === undefined
+      ? null
+      : reuseCookieRef.current?.checked
+      ? toHandle.encrypted
+      : null;
+  const paramsJson = paramsRef.current!.value || null;
+  const params = paramsJson ? JSON.parse(paramsJson) : {};
+  const cookie = cookieRef.current!.value || null;
+  if (cookie) params.cookie = cookie;
+  const data_input = {
+    url: urlRef.current!.value,
+    params,
+    selector: cssSelectorRef.current!.value || null,
+    evaluation: null,
+    evaluate: evaluateRef.current!.value || null,
+    evaluation_to_img: evaluationToImgRef.current!.checked,
+  };
+  if (data_input.url === "") {
+    throw Error("need to have a url");
+  }
+  return { old_encrypted, ...data_input };
 }
 
 export default ToHandle;
