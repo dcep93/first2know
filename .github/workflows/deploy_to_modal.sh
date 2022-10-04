ea91cb52cc2710173ee9b0fc34abf37d2a2b553e
dcep93 <dcep93@gmail.com>
2022-10-04 11:49:11 -0400

    x

--- .github/workflows/deploy_to_modal.sh
+++ .github/workflows/deploy_to_modal.sh
@@ -6,9 +6,8 @@ MODAL_KEY="$1"
 TWITTER_KEY="$2"
 
 TOKEN_ID="ak-38vXak6m5VJheg1cvrKQul"
-MODAL_PIP_URL="https://modal.com/api/client-library/us-JOXmaxhr5FVrM66sBK1J29/modal_client-0.35.1913-py3-none-any.whl"
 
-pip install "$MODAL_PIP_URL"
+pip install modal-client
 modal token set --token-id "$TOKEN_ID" --token-secret "$MODAL_KEY"
 
 cd ../../backend
