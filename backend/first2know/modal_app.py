d1ecddf7a42bac38cb10b02a30f7c1f909188fa9
dcep93 <dcep93@gmail.com>
2022-10-08 17:17:39 -0400

    todo

--- backend/first2know/modal_app.py
+++ backend/first2know/modal_app.py
@@ -23,7 +23,6 @@ def init(s: str):
     secrets.Vars.secrets = secrets.Secrets(**json.loads(raw_json))
 
 
-# TODO akshat - can the first run always be NOW, not NOW + modal.Period
 @modal_app.function(
     schedule=modal.Period(seconds=PERIOD_SECONDS),
     secret=modal.Secret.from_name("first2know_s"),
