fee7a3368a4958b21c9b141773f817078d917476
dcep93 <dcep93@gmail.com>
2023-04-25 21:54:10 -0400

    oneoff

--- backend/first2know/oneoff.py
+++ backend/first2know/oneoff.py
@@ -5,15 +5,16 @@ from . import screenshot
 
 def main():
     url = "https://www.stubhub.com/taylor-swift-east-rutherford-tickets-5-26-2023/event/150593661/"
-    request = screenshot.Request(
-        data_input=firebase_wrapper.DataInput(
-            url=url,
-            # params={"data": {
-            #     "Quantity": 1
-            # }},
-        ), )
+    request = screenshot.Request(data_input=firebase_wrapper.DataInput(
+        url=url,
+        raw_proxy=True,
+        params={"data": {
+            "Quantity": 1,
+        }},
+        evaluate="document.body.innerHTML",
+    ), )
     screenshot_response = screenshot.Screenshot()(request)
-    print(screenshot_response)
+    print(screenshot_response.evaluation)
     print("oneoff complete")
 
 
