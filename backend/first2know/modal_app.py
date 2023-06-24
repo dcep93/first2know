0fef6e2241b4757335e30f18188021b1b7682196
dcep93 <dcep93@gmail.com>
2023-06-24 15:46:41 -0400

    x

--- backend/first2know/modal_app.py
+++ backend/first2know/modal_app.py
@@ -25,6 +25,7 @@ def init(s: str):
     secret=modal.Secret.from_name("first2know_s"),
 )
 def modal_cron():
+    return
     from . import cron
     init("cron")
     was_successful = cron.loop(PERIOD_SECONDS, GRACE_PERIOD_SECONDS)
