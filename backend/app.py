cbb5258e6383421b8da856db50e35d2b3eb996da
dcep93 <dcep93@gmail.com>
2024-06-15 16:20:57 -0700

    x

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI, Request, Response  # type: ignore
 from fastapi.responses import HTMLResponse  # type: ignore
 
@@ -8,6 +9,9 @@ web_app = FastAPI()
 @web_app.get("/")
 def get_(request: Request):
     return HTMLResponse("hi")
+
+
+uvicorn.run(web_app, host="0.0.0.0", port=8080),
 
 
 # import threading
