bb76c1da3f648eb54235105fbb404675ba5d084f
dcep93 <dcep93@gmail.com>
2024-06-15 15:07:22 -0700

    x

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,11 @@
+import threading
 import uvicorn
-import concurrent.futures
 
 from . import cron, server
 
-futures.ThreadPoolExecutor(max_workers=8).submit(
-    lambda: uvicorn.run(server.web_app, host="0.0.0.0", port=8000)
-)
+threading.Thread(
+    target=lambda: uvicorn.run(server.web_app, host="0.0.0.0", port=8000),
+    daemon=True,
+).start()
 cron.loop()
 raise Exception("loop_finished")
