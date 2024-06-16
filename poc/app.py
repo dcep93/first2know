f4087ba6a1fc4a306d5d256f63b09c8cbed9fe01
dcep93 <dcep93@gmail.com>
2024-06-16 11:39:22 -0700

    x

--- poc/app.py
+++ poc/app.py
@@ -11,4 +11,14 @@ def get_(request: Request):
     return HTMLResponse("hi")
 
 
+@web_app.get("/health")
+def get_health(request: Request):
+    return HTMLResponse("hi")
+
+
+@web_app.get("/healthcheck")
+def get_healthcheck(request: Request):
+    return HTMLResponse("hi")
+
+
 uvicorn.run(web_app, host="0.0.0.0", port=8080)
