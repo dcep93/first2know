27de7a51e1e3412a1564c6ea4aa48e3a7bbec27e
dcep93 <dcep93@gmail.com>
2024-06-16 18:54:50 -0700

    x

--- backend/first2know/screenshot.py
+++ backend/first2know/screenshot.py
@@ -18,7 +18,7 @@ from . import secrets
 
 import nest_asyncio  # type: ignore
 
-nest_asyncio.apply()
+# nest_asyncio.apply()
 
 GOOD_USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"  # noqa
 
