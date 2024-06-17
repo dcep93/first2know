e068e965b0baf4ca8deed9db8f2708242abbb7d6
dcep93 <dcep93@gmail.com>
2024-06-16 18:32:07 -0700

    x

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,13 @@
-from first2know import cron, server
+# from first2know import cron, server
 
-app = server.web_app
+# app = server.web_app
+
+from fastapi import FastAPI, Request, Response  # type: ignore
+from fastapi.responses import HTMLResponse  # type: ignore
+
+app = FastAPI()
+
+
+@app.get("/")
+def get_(request):
+    return HTMLResponse("hi")
