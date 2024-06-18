97143480e8435e6044b1cde06883241b6ea6f766
dcep93 <dcep93@gmail.com>
2024-06-18 00:43:46 -0700

    x

--- backend/app.py
+++ backend/app.py
@@ -3,16 +3,10 @@ import time
 
 from first2know import cron, server
 
-
-def start_server():
-    time.sleep(10)
-    server.init()
-    print("cron.loop_with_manager")
-    cron.loop_with_manager(server.Vars.screenshot_manager)
-
+server.init()
 
 threading.Thread(
-    target=start_server,
+    target=lambda: cron.loop_with_manager(server.Vars.screenshot_manager),
     daemon=True,
 ).start()
 
