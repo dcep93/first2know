e506f63c86e3153898a5f345ddff2d5a125f849e
dcep93 <dcep93@gmail.com>
2024-06-25 23:33:06 -0400

    x

--- .github/workflows/deploy_backend.sh
+++ .github/workflows/deploy_backend.sh
@@ -7,17 +7,19 @@ SECRETS_JSON="$2"
 
 cd "$(dirname "$0")"
 
-DIFF="$(git diff HEAD^ ../../backend/first2know)"
-if [[ -z "$DIFF" ]]; then
-  echo "no diff"
-else
-  echo "$DIFF"
-  bash ./record_sha.sh "recorded_sha = '''%s\n%s'''\n" "../../backend/first2know/recorded_sha.py"
-  bash ./deploy_to_app_engine.sh "$SA_KEY" "$SECRETS_JSON"
-fi
-
 function c() {
   curl --fail https://first2know20240615.appspot.com/
 }
+
+DIFF="$(git diff HEAD^ ../../backend/first2know)"
+if [[ -z "$DIFF" ]]; then
+  echo "no diff"
+  c && exit 0 || true
+else
+  echo "$DIFF"
+fi
+
+bash ./record_sha.sh "recorded_sha = '''%s\n%s'''\n" "../../backend/first2know/recorded_sha.py"
+bash ./deploy_to_app_engine.sh "$SA_KEY" "$SECRETS_JSON"
 
 c || c || c
