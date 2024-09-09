3693dcf99099d423d4981eed14a22ec6628588c4
dcep93 <dcep93@gmail.com>
2024-09-09 13:10:42 -0400

    x

--- .github/workflows/deploy_to_firebase.sh
+++ .github/workflows/deploy_to_firebase.sh
@@ -6,14 +6,14 @@ SA_KEY="$1"
 
 # GOOGLE_CLOUD_PROJECT=first2know
 # firebase init hosting --project "$GOOGLE_CLOUD_PROJECT"
-# gcloud iam service-accounts create deployer-github
-# gcloud projects add-iam-policy-binding "$GOOGLE_CLOUD_PROJECT" --member="serviceAccount:deployer-github@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com" --role="roles/firebasehosting.admin"
-# gcloud iam service-accounts keys create gac.json --iam-account "deployer-github@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com"
+# gcloud iam service-accounts create firebase-deployer-github
+# gcloud projects add-iam-policy-binding "$GOOGLE_CLOUD_PROJECT" --member="serviceAccount:firebase-deployer-github@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com" --role="roles/firebasehosting.admin"
+# gcloud iam service-accounts keys create gac.json --iam-account "firebase-deployer-github@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com"
 # cat gac.json
 
 cd ../../frontend/first2know
 export GOOGLE_APPLICATION_CREDENTIALS="gac.json"
-echo "$SA_KEY" > "$GOOGLE_APPLICATION_CREDENTIALS"
+echo "$SA_KEY" >"$GOOGLE_APPLICATION_CREDENTIALS"
 npm install -g firebase-tools
 gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
 firebase deploy --project "$(cat $GOOGLE_APPLICATION_CREDENTIALS | jq -r .project_id)"
--- backend/first2know/exceptions.py
+++ backend/first2know/exceptions.py
@@ -20,15 +20,17 @@ def get_ignorable_exception(
     # print('\n'.join(dir(e)))
     if src == Src.playwright_screenshot:
         if str(e.__class__
+               ) == "<class 'asyncio.exceptions.TimeoutError'>":
+            return IgnorableException("asyncio_timeout")
+        if str(e.__class__
                ) == "<class 'playwright._impl._api_types.TimeoutError'>":
             return IgnorableException("playwright_timeout")
         if str(e.__class__
                ) == "<class 'playwright._impl._api_types.Error'>":
             if e.message.startswith("Browser closed.\n"):  # type: ignore
                 return IgnorableException("browser_closed")
-        if str(e.__class__
-               ) == "<class 'asyncio.exceptions.TimeoutError'>":
-            return IgnorableException("asyncio_timeout")
+            if e.message.startswith("net::ERR_CONNECTION_RESET at"):  # type: ignore
+                return IgnorableException("connection_reset")
 
     if src == Src.screenshot_null_location:
         if str(e.__class__) == "<class 'playwright._impl._api_types.Error'>":
