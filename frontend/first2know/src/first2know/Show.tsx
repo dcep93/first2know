ab4429ab88063d79a2be49c68357755078bc0862
dcep93 <dcep93@gmail.com>
2025-03-16 15:17:32 -0400

    x

--- frontend/first2know/src/first2know/Show.tsx
+++ frontend/first2know/src/first2know/Show.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 import { AllToHandleType, ToHandleType, UserType } from "./firebase";
 import styles from "./index.module.css";
@@ -22,20 +21,11 @@ function Show(props: {
 }
 
 export function RenderToHandle(props: { k: string; toHandle: ToHandleType }) {
-  const [hidden, update] = useState(true);
   return (
     <div>
       <pre className={styles.toHandlePre}>
         {JSON.stringify(props.toHandle, null, 2)}
       </pre>
-      <div>
-        <button onClick={() => update(!hidden)}>Toggle Output</button>
-      </div>
-      <div hidden={hidden}>
-        {props.toHandle.data_output?.error && (
-          <pre>{JSON.stringify(props.toHandle.data_output.error, null, 2)}</pre>
-        )}
-      </div>
     </div>
   );
 }
