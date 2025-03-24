c75922a5304c6835ab9a6281473087a4ee55420c
Daniel Cepeda <dcep93@gmail.com>
2025-03-24 19:28:00 -0400

    x

--- .github/workflows/load_backend_cache.sh
+++ .github/workflows/load_backend_cache.sh
@@ -9,9 +9,7 @@ cd ../../backend
 mkdir -p /tmp/github-cache
 ls -lah /tmp/github-cache || true
 
-docker buildx build --cache-to=type=registry,ref=myrepo/myimage-cache --tag myimage --push . || echo failed
-
-rm /tmp/github-cache/*
+docker buildx build --cache-to=type=local,dest=/tmp/github-cache --cache-from=type=local,src=/tmp/github-cache .
 
 docker image ls
 
