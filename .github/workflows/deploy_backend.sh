0cb76a6098f7021e0f6aed138b07a790b5e51e20
Daniel Cepeda <dcep93@gmail.com>
2025-03-24 19:23:10 -0400

    x

--- .github/workflows/deploy_backend.sh
+++ .github/workflows/deploy_backend.sh
@@ -14,15 +14,15 @@ function c() {
 DIFF="$(git diff HEAD^ ../../backend)"
 if [[ -z "$DIFF" ]]; then
   echo "no diff"
-  # c && exit 0 || true
+  c && exit 0 || true
 else
   echo "$DIFF"
 fi
 
 bash ./load_backend_cache.sh
-# bash ./record_sha.sh "recorded_sha = '''%s\n%s'''\n" "../../backend/first2know/recorded_sha.py"
-# bash ./record_secret.sh "$SECRETS_JSON"
-# bash ./test_backend.sh
-# bash ./deploy_to_app_engine.sh "$SA_KEY"
+bash ./record_sha.sh "recorded_sha = '''%s\n%s'''\n" "../../backend/first2know/recorded_sha.py"
+bash ./record_secret.sh "$SECRETS_JSON"
+bash ./test_backend.sh
+bash ./deploy_to_app_engine.sh "$SA_KEY"
 
 c || c || c
--- .github/workflows/load_backend_cache.sh
+++ .github/workflows/load_backend_cache.sh
@@ -6,9 +6,13 @@ cd ../../backend
 
 mkdir -p /tmp/github-cache
 ls -lah /tmp/github-cache || true
-test ! -f /tmp/github-cache/backend.tar || (docker load -i /tmp/github-cache/backend.tar && echo loaded)
+
+docker buildx build --cache-to=type=registry,ref=myrepo/myimage-cache --tag myimage --push . || echo failed
+
+rm /tmp/github-cache/*
+
 docker image ls
 docker history first2know
 make dockerbuild
-docker save -o /tmp/github-cache/backend.tar first2know:latest
+# docker save -o /tmp/github-cache/backend.tar first2know:latest
 ls -lah /tmp/github-cache || true
