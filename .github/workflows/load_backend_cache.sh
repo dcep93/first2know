08040ee81e3115809828935abdacd5847d04a9a6
Daniel Cepeda <dcep93@gmail.com>
2025-03-24 19:14:45 -0400

    x

--- .github/workflows/load_backend_cache.sh
+++ .github/workflows/load_backend_cache.sh
@@ -8,5 +8,6 @@ mkdir -p /tmp/github-cache
 ls -lah /tmp/github-cache || true
 test ! -f /tmp/github-cache/backend.tar || (docker load -i /tmp/github-cache/backend.tar && echo loaded)
 make dockerbuild
+make dockerbuild
 docker save -o /tmp/github-cache/backend.tar first2know:latest
 ls -lah /tmp/github-cache || true
