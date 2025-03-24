528ee4a704e4bc63c3a080535daa9c72293ea5d6
Daniel Cepeda <dcep93@gmail.com>
2025-03-24 19:40:27 -0400

    x

--- .github/workflows/load_backend_cache.sh
+++ .github/workflows/load_backend_cache.sh
@@ -9,17 +9,17 @@ cd ../../backend
 mkdir -p /tmp/github-cache
 ls -lah /tmp/github-cache || true
 
-docker image ls
+docker image ls -a
 
 docker buildx build --cache-to=type=local,dest=/tmp/github-cache --cache-from=type=local,src=/tmp/github-cache --load .
 
 echo asdfasdf
 
-docker image ls
+docker image ls -a
 
 make dockerbuild
 
-rm /tmp/github-cache/butt*
+rm /tmp/github-cache/butt* || true
 
 touch "/tmp/github-cache/butt_$(date +%s)"
 
