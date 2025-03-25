c3b9c01d532f7b08d5012f20c6ca37e760ce4f10
Daniel Cepeda <dcep93@gmail.com>
2025-03-24 20:32:18 -0400

    x

--- .github/workflows/cache_backend_docker.sh
+++ .github/workflows/cache_backend_docker.sh
@@ -11,7 +11,7 @@ docker image ls -a
 docker buildx build \
     --cache-to=type=local,dest=/tmp/github-cache \
     --cache-from=type=local,src=/tmp/github-cache \
-    --load .
+    --load -t first2know:latest .
 
 echo asdfasdf
 
