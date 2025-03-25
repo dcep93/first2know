70923c822d586115f4f0b0c8bf55764df4b114a4
Daniel Cepeda <dcep93@gmail.com>
2025-03-24 20:57:45 -0400

    x

--- .github/workflows/cache_backend_docker.sh
+++ .github/workflows/cache_backend_docker.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+set -x
+
 cd "$(dirname "$0")"
 
 cd ../../backend
@@ -9,13 +11,15 @@ cd ../../backend
 docker image ls -a
 
 docker buildx build \
-    --cache-to=type=local,dest=/tmp/github-cache/backend \
-    --cache-from=type=local,src=/tmp/github-cache/backend \
     --load --platform linux/amd64 -t first2know:latest .
 
+
+    # --cache-to=type=local,dest=/tmp/github-cache/backend \
+    # --cache-from=type=local,src=/tmp/github-cache/backend \
+
+echo history
 docker history first2know
 
-echo asdfasdf
 
 docker image ls -a
 
