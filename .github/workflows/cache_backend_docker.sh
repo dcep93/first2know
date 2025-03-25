0c69a842d1ad06b459aa9388b4775ee5a3b5dd2d
Daniel Cepeda <dcep93@gmail.com>
2025-03-24 21:29:03 -0400

    x

--- .github/workflows/cache_backend_docker.sh
+++ .github/workflows/cache_backend_docker.sh
@@ -10,10 +10,10 @@ cd ../../backend
 
 docker image ls -a
 
-docker buildx build --no-cache \
+docker buildx build \
     --cache-to=type=local,dest=/tmp/github-cache/backend \
+    --cache-from=type=local,src=/tmp/github-cache/backend \
     --load --platform linux/amd64 -t first2know:buildx .
-    # --cache-from=type=local,src=/tmp/github-cache/backend \
 
 docker image ls -a
 
