9f3dcff181c0c8750a16602cbbb810339590753e
Daniel Cepeda <dcep93@gmail.com>
2025-03-24 21:40:16 -0400

    x

--- .github/workflows/cache_backend_docker.sh
+++ .github/workflows/cache_backend_docker.sh
@@ -7,6 +7,6 @@ cd ../../backend
 docker buildx build \
     --cache-to=type=local,dest=/tmp/github-cache/backend \
     --cache-from=type=local,src=/tmp/github-cache/backend \
-    --load --platform linux/amd64 -t first2know:latest .
+    -t first2know:latest .
 
 make dockerbuild
