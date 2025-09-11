4fddfe6b6b961dad8b32d00effd7f1293095596c
Daniel Cepeda <dcep93@gmail.com>
2025-09-11 11:00:32 -0400

    x

--- frontend/first2know/src/first2know/firebase/crypt.ts
+++ frontend/first2know/src/first2know/firebase/crypt.ts
@@ -1,18 +1,12 @@
-import { Secret, Token } from "fernet";
+import { Fernet } from "fernet-nodejs";
 
-function encrypt(unencrypted_string: string, secret: Secret): string {
-  const token = new Token({ secret, time: Date.now() });
-  return token.encode(unencrypted_string);
+function encrypt(unencrypted_string: string, fernet_key_str: string): string {
+  return Fernet.encrypt(unencrypted_string, fernet_key_str);
 }
 
-function decrypt(encrypted_string: string, secret: Secret): string {
-  const token = new Token({ secret, token: encrypted_string, ttl: 0 });
-  return token.decode();
+function decrypt(encrypted_string: string, fernet_key_str: string): string {
+  return Fernet.decrypt(encrypted_string, fernet_key_str);
 }
 
-function getSecret(fernet_key_str: string): Secret {
-  return new Secret(fernet_key_str);
-}
-
-const vars = { encrypt, decrypt, getSecret };
+const vars = { encrypt, decrypt };
 export default vars;
