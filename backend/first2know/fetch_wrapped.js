ac00deb68165a429ce79ee6ca5e7fa8cb39ac7f7
Daniel Cepeda <dcep93@gmail.com>
2025-09-12 13:20:23 -0400

    x

--- backend/first2know/fetch_wrapped.js
+++ backend/first2know/fetch_wrapped.js
@@ -2,6 +2,11 @@ Promise.resolve().then(() => {
   //
   function first2knowF(currentYear) {
     const leagueId = 203836968;
+    function fromEntries(arr) {
+      return Object.fromEntries(
+        arr.filter((a) => a !== undefined).map((a) => [a.key, a.value])
+      );
+    }
     return Promise.resolve()
       .then(() => [
         Promise.resolve()
@@ -28,9 +33,75 @@ Promise.resolve().then(() => {
                 },
                 credentials: "include",
               }
-            ).then((resp) => resp.json())
+            )
+              .then((resp) => resp.json())
+              .then((resp) =>
+                resp.players
+                  .map((player) => player.player)
+                  .map((player) => ({
+                    player,
+                    seasonStats: player.stats.find(
+                      (s) => s.scoringPeriodId === 0 && s.statSourceId === 0
+                    ),
+                  }))
+                  .map(({ player, seasonStats }) => ({
+                    id: player.id.toString(),
+                    nflTeamId: player.proTeamId.toString(),
+                    name: player.fullName,
+                    position:
+                      { 1: "QB", 2: "RB", 3: "WR", 4: "TE", 5: "K", 16: "DST" }[
+                        player.defaultPositionId
+                      ] || player.defaultPositionId.toString(),
+                    total:
+                      (seasonStats === null || seasonStats === void 0
+                        ? void 0
+                        : seasonStats.appliedTotal) || 0,
+                    average:
+                      (seasonStats === null || seasonStats === void 0
+                        ? void 0
+                        : seasonStats.appliedAverage) || 0,
+                    scores: fromEntries(
+                      player.stats
+                        .filter(
+                          (stat) =>
+                            stat.seasonId === parseInt(currentYear) &&
+                            stat.statSourceId === 0
+                        )
+                        .map((stat) => ({
+                          key: stat.scoringPeriodId.toString(),
+                          value: parseFloat(
+                            (stat.appliedTotal || 0).toFixed(2)
+                          ),
+                        }))
+                    ),
+                    ownership: Object.fromEntries(
+                      Object.entries(player.ownership || {}).filter(([k]) =>
+                        [
+                          "averageDraftPosition",
+                          "auctionValueAverage",
+                          "percentOwned",
+                        ].includes(k)
+                      )
+                    ),
+                    injuryStatus: player.injuryStatus,
+                  }))
+                  .filter((player) => {
+                    var _player$ownership;
+                    return (
+                      Object.keys(player.ownership).length > 0 &&
+                      (((_player$ownership = player.ownership) === null ||
+                      _player$ownership === void 0
+                        ? void 0
+                        : _player$ownership.percentOwned) > 0.1 ||
+                        Object.values(player.scores).filter((s) => s !== 0)
+                          .length > 0)
+                    );
+                  })
+                  .map(({ ...player }) => ({ key: player.id, value: player }))
+              )
+              .then((playersArr) => fromEntries(playersArr))
           )
-          .then((v) => ["nflPlayersSource", v]),
+          .then((v) => ["nflPlayers", v]),
         Promise.resolve()
           .then(() =>
             fetch(
@@ -55,8 +126,119 @@ Promise.resolve().then(() => {
                   .then((ps) => Promise.all(ps))
                   .then((weeks) => ({ main, weeks }))
               )
+              .then(({ main, weeks }) =>
+                Promise.resolve()
+                  .then(() =>
+                    weeks.map(({ week, weekNum }) =>
+                      Promise.resolve()
+                        .then(() =>
+                          week.teams.map((team) => ({
+                            id: team.id.toString(),
+                            name: team.name,
+                            schedule: {
+                              weekNum,
+                              ...week.schedule
+                                .flatMap((matchup) => [
+                                  matchup.home,
+                                  matchup.away,
+                                ])
+                                .find(
+                                  (s) =>
+                                    (s === null || s === void 0
+                                      ? void 0
+                                      : s.rosterForCurrentScoringPeriod) &&
+                                    s.teamId === team.id
+                                ),
+                            },
+                          }))
+                        )
+                        .then((week) =>
+                          fromEntries(
+                            week.map((team) => ({ key: team.id, value: team }))
+                          )
+                        )
+                    )
+                  )
+                  .then((ps) => Promise.all(ps))
+                  .then((weeks) =>
+                    Object.values(weeks[0] || {}).map((team) => ({
+                      id: team.id,
+                      name: team.name,
+                      draft: main.draftDetail.picks
+                        .map((p, pickIndex) => ({ ...p, pickIndex }))
+                        .filter((p) => p.teamId === parseInt(team.id))
+                        .map(({ playerId, pickIndex }) => ({
+                          playerId,
+                          pickIndex,
+                        })),
+                      pickOrder: main.settings.draftSettings.pickOrder.indexOf(
+                        parseInt(team.id)
+                      ),
+                      rosters: fromEntries(
+                        weeks
+                          .map((week) => week[team.id].schedule)
+                          .filter((s) => s.rosterForCurrentScoringPeriod)
+                          .map((s) => ({
+                            weekNum: s.weekNum.toString(),
+                            starting: s.rosterForCurrentScoringPeriod.entries
+                              .filter((e) => ![20, 21].includes(e.lineupSlotId))
+                              .map((e) => e.playerId.toString()),
+                            rostered:
+                              s.rosterForCurrentScoringPeriod.entries.map((e) =>
+                                e.playerId.toString()
+                              ),
+                          }))
+                          .concat({
+                            weekNum: "0",
+                            starting: [],
+                            rostered: main.teams
+                              .find((t) => t.id.toString() === team.id)
+                              .roster.entries.map((e) => e.playerId.toString()),
+                          })
+                          .map((roster) => ({
+                            key: roster.weekNum,
+                            value: roster,
+                          }))
+                      ),
+                    }))
+                  )
+                  .then((teams) =>
+                    fromEntries(
+                      teams.map((team) => ({ key: team.id, value: team }))
+                    )
+                  )
+              )
+              .then((teams) => teams)
           )
-          .then((v) => ["ffTeamsSource", v]),
+          .then((v) => ["ffTeams", v]),
+        Promise.resolve()
+          .then(() =>
+            fetch(
+              `https://lm-api-reads.fantasy.espn.com/apis/v3/games/ffl/seasons/${currentYear}/segments/0/leagues/${leagueId}?view=mMatchupScore&view=mSettings`,
+              { credentials: "include" }
+            )
+              .then((resp) => resp.json())
+              .then((resp) =>
+                Array.from(
+                  new Array(resp.settings.scheduleSettings.matchupPeriodCount)
+                )
+                  .map((_, i) => i + 1)
+                  .map((matchupPeriodId) => ({
+                    key: matchupPeriodId.toString(),
+                    value: resp.schedule
+                      .filter((s) => s.matchupPeriodId === matchupPeriodId)
+                      .map((s) =>
+                        [s.home, s.away].map((t) =>
+                          t === null || t === void 0
+                            ? void 0
+                            : t.teamId.toString()
+                        )
+                      ),
+                  }))
+              )
+              .then((matchups) => fromEntries(matchups))
+          )
+          .then((v) => ["ffMatchups", v]),
         Promise.resolve()
           .then(() =>
             fetch(
@@ -93,5 +275,6 @@ Promise.resolve().then(() => {
       .then(Object.fromEntries);
   }
   return first2knowF("2025");
+
   //
 });
