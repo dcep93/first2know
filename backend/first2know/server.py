b6ee1fd23d5a87a7b34084c320e8fd8b21fb04ba
Daniel Cepeda <dcep93@gmail.com>
2025-09-15 11:53:01 -0400

    x

--- backend/first2know/server.py
+++ backend/first2know/server.py
@@ -19,7 +19,7 @@ from . import proxy
 from . import recorded_sha
 from . import screenshot
 
-NUM_SCREENSHOTTERS = 3
+NUM_SCREENSHOTTERS = 1
 MAX_CRON_AGE = 300
 
 process = cron.psutil.Process(cron.os.getpid())
